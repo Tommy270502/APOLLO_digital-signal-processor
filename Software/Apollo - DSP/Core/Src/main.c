3ba7b862b8ef4199d300daaa310bd2a312cd22a7
Tommy270502 <perrithomas@outlook.com>
2023-03-30 00:13:58 +0200

    added boot and reset button

--- Software/Apollo - DSP/Core/Src/main.c
+++ Software/Apollo - DSP/Core/Src/main.c
@@ -129,7 +129,6 @@ int main(void)
 
 	  	timerUSB = HAL_GetTick();
 	}
-
 	HAL_Delay(10);
   }
   /* USER CODE END 3 */
@@ -360,12 +359,16 @@ static void MX_DMA_Init(void)
   */
 static void MX_GPIO_Init(void)
 {
+/* USER CODE BEGIN MX_GPIO_Init_1 */
+/* USER CODE END MX_GPIO_Init_1 */
 
   /* GPIO Ports Clock Enable */
   __HAL_RCC_GPIOH_CLK_ENABLE();
   __HAL_RCC_GPIOA_CLK_ENABLE();
   __HAL_RCC_GPIOB_CLK_ENABLE();
 
+/* USER CODE BEGIN MX_GPIO_Init_2 */
+/* USER CODE END MX_GPIO_Init_2 */
 }
 
 /* USER CODE BEGIN 4 */
