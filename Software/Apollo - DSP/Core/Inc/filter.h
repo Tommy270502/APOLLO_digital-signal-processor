6eb621ed5eafe77ea0251b9980f0143226cca5ae
Tommy270502 <perrithomas@outlook.com>
2023-05-15 22:10:45 +0200

    added sram chip again, might come in handy for mor complex calculations

---
+++ Software/Apollo - DSP/Core/Inc/filter.h
@@ -0,0 +1,32 @@
+/*
+ * filter.h
+ *
+ *  Created on: Feb 3, 2023
+ *      Author: Perri
+ */
+
+#ifndef INC_FILTER_H_
+#define INC_FILTER_H_
+
+typedef struct {
+
+	float alpha[2];
+	float out[2];
+
+}LowPassFilter;
+
+typedef struct {
+
+	float alpha[2];
+	float input[2];
+	float out[2];
+
+}HighPassFilter;
+
+void init_HighPassFilter(HighPassFilter *handle, float cutoffFreq, float sampletimeS);
+void update_HighPassFilter(HighPassFilter *handle, float input);
+
+void init_LowPassFilter(LowPassFilter *handle, float cutoffFreq, float sampletimeS);
+void update_LowPassFilter(LowPassFilter *handle, float input);
+
+#endif
