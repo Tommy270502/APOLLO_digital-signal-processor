5fe934bdc8eede7bcf3c0c3f3801f483a9d6f1ab
Tommy270502 <perrithomas@outlook.com>
2023-05-27 09:02:34 +0200

    testram function finished

--- Software/Apollo - DSP/Core/Src/23K256.c
+++ Software/Apollo - DSP/Core/Src/23K256.c
@@ -14,9 +14,9 @@ void set_RAM_Mode(SPI_HandleTypeDef *spi, uint8_t MODE) {
 	HAL_GPIO_WritePin(GPIOA, GPIO_PIN_2, GPIO_PIN_SET);
 }
 
-void testRAM(SPI_HandleTypeDef *spi) {
+uint8_t testRAM(SPI_HandleTypeDef *spi) {
 	uint8_t RX_Data = 0;
-
+	uint8_t err = 0;
 	HAL_GPIO_WritePin(GPIOA, GPIO_PIN_2, GPIO_PIN_RESET);
 	HAL_SPI_Transmit(spi, WRSR, sizeof(WRSR), 100);
 	HAL_SPI_Transmit(spi, SEQUENTIAL_MODE, sizeof(SEQUENTIAL_MODE), 100);
@@ -43,8 +43,12 @@ void testRAM(SPI_HandleTypeDef *spi) {
 	HAL_SPI_Transmit(spi, 0, sizeof(0), 100);
 	for(i=0; i<32767; i++) {
 		HAL_SPI_Receive(&hspi1, RX_Data, sizeof(RX_Data), 100);
+		if(data == 0) {
+			err = 1;
+		}
 	}
 	HAL_GPIO_WritePin(GPIOA, GPIO_PIN_2, GPIO_PIN_SET);
+	return err;
 }
 
 void writeByteRAM(SPI_HandleTypeDef *spi, uint16_t address, uint8_t data) {
